2d7a652d07734fd619305a9cb8cc859e69c930bb
Esteban Gomez <egomezl29@miumg.edu.gt>
2025-03-17 16:57:35 -0600

    First table: Genero

--- DB_Grupo1.sql
+++ DB_Grupo1.sql
@@ -3,3 +3,10 @@
 CREATE DATABASE IF NOT EXISTS DB_Grupo1;
 USE DB_Grupo1;
 /*DROP DATABASE db_grupo1;*/
+
+CREATE TABLE Genero(
+	id_genero INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_genero VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(250) NOT NULL,
+    ventas_totales int
+);
